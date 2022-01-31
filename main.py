5d3700ed4cf07797a0af6d01945166ae0d15d0ea
Tesl1k <danildavydov161@mail.ru>
2022-01-31 08:20:08 +0300

    Заготовка 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import os
+import os
 
 d = {}
 def a():
@@ -15,8 +15,7 @@ def d():
 
 #точка ввода
 print('/T\\')
-print('|.|  Привет я точка в'
-      '[входа, напиши "start" чтобы начать')
+print('|.|  Привет я точка входа, напиши "start" чтобы начать')
 while True:
     vhod = input()
     if vhod =="start":
